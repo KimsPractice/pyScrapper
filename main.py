f52d284a4bb625c9a442fd5eb34d4915494a60a5
kimspractice <tpghksdldia@naver.com>
2021-02-01 10:22:26 +0900

    객체지향형으로 만듬

--- main.py
+++ main.py
@@ -5,31 +5,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-KEYWORD = "buy domain"
+class GoogleKeywordScreenshooter():
+    def __init__(self,keyword,screenshots_dir):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())  
+        self.keyword = keyword
+        self.screenshots_dir = screenshots_dir
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+    def start(self):
+        self.browser.get("http://google.com")
+        search_bar = self.browser.find_element_by_class_name("gLFyf")
+        search_bar.send_keys(self.keyword)
+        search_bar.send_keys(Keys.ENTER)
 
-browser.get("http://google.com")
+        try:
+            shitty_element = WebDriverWait(self.browser,10).until(EC.presence_of_element_located((By.CLASS_NAME, "g-blk")))
 
-search_bar = browser.find_element_by_class_name("gLFyf")
+            self.browser.execute_script(
+                """
+                const shitty = arguments[0]
+                console.log(shitty)
+                shitty.parentElement.removeChild(shitty)
+            """,shitty_element)
+        except:
+            pass
 
-search_bar.send_keys(KEYWORD)
-search_bar.send_keys(Keys.ENTER)
+        search_results = self.browser.find_elements_by_class_name("g")
 
-shitty_element = WebDriverWait(browser,10).until(EC.presence_of_element_located((By.CLASS_NAME, "g-blk")))
-
-browser.execute_script(
-    """
-    const shitty = arguments[0]
-    console.log(shitty)
-    shitty.parentElement.removeChild(shitty)
-""",shitty_element)
-
-search_results = browser.find_elements_by_class_name("g")
-
-for index,search_result in enumerate(search_results):
-    class_name = search_result.get_attribute("class")
-    if "kno-kp mnr-c g-blk" not in class_name:
-        search_result.screenshot(f"screenshots/{KEYWORD}x{index}.png")
+        for index,search_result in enumerate(search_results):
+            class_name = search_result.get_attribute("class")
+            if "kno-kp mnr-c g-blk" not in class_name:
+                search_result.screenshot(f"{self.screenshots_dir}/{self.keyword}x{index}.png")
     
-browser.quit()
+    def end(self):
+        self.browser.quit()
+
+
+googleKeyword_Screenshooter = GoogleKeywordScreenshooter("python book","screenshots")
+googleKeyword_Screenshooter.start()
+googleKeyword_Screenshooter.end()
